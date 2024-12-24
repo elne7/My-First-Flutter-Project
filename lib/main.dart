db1a6b91191e0860512f2a641c2ee38f60188f86
ahmedelne7 <ahmedelnahrawy881@gmail.com>
2024-12-24 16:15:57 +0200

    finishing the task

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,10 @@ class TheFirstFlutterProject extends StatelessWidget {
     return MaterialApp(
       title: 'The first flutter project',
       home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("My First Project"),
+        ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -19,17 +23,27 @@ class TheFirstFlutterProject extends StatelessWidget {
               children: [
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Image.asset("assets/images/pic1.jpg", width: 150,),
+                  child: Image.asset(
+                    "assets/images/pic1.jpg",
+                    width: 150,
+                  ),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Image.asset("assets/images/pic2.jpg", width: 150,),
+                  child: Image.network(
+                    "https://picsum.photos/200",
+                    width: 150,
+                  ),
                 ),
               ],
             ),
             const Text(
-              "Hello!",
-              style: TextStyle(fontSize: 24),
+              "The two images are displayed!",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontFamily: "Suwannaphum",
+              ),
             )
           ],
         ),
